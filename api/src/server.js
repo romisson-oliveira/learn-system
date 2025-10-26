9ef9e9a5e2e42c29d7109ff202bb031dfaa8ceb8
Romisson Oliveira <oliveiraromisson@hotmail.com>
2025-10-26 18:57:44 -0300

    refactor: using prisma.user.create and prisma.user.findMany

--- api/src/server.js
+++ api/src/server.js
@@ -1,30 +1,38 @@
 import express from "express";
-const app = express();
-const PORT = 3000 || process.env.PORT; // process.env.PORT pega a porta do ambiente de dev. como Replit p.ex.
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+const app = express();
 // Para express poder trabalhar com json
 app.use(express.json());
 
-// Simulando usuários
-const users = [];
-
 // Criar usuários
-app.post("/usuarios", (req, res) => {
-  const { name } = req.body;
-
-  users.push(req.body);
+app.post("/usuarios", async (req, res) => {
+  // const { name, email, age } = req.body;
+  // Utiliznado o prisma para enviar a criação dos dados para o banco de dados configurado
+  // prisma.user -> porque nosso schema tem o nome de user
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
 
   // Ao cadastrar usuário, o front end obtém uma resposta:
-  res.send(`Usuário ${name} cadastrado com sucesso!`);
+  res.status(201).json(req.body);
 });
 
 // Listar todos os usuários
-app.get("/usuarios", (req, res) => {
-  res.send(users);
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.status(200).json(users);
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
 });
 
 /* 
