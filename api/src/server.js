9813f30b0aecd7dc950979bc4df778e4b3cddf08
Romisson Oliveira <oliveiraromisson@hotmail.com>
2025-10-25 23:32:22 -0300

    feat: add user creation and list all users

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,36 @@
 import express from "express";
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = 3000 || process.env.PORT; // process.env.PORT pega a porta do ambiente de dev. como Replit p.ex.
 
+// Para express poder trabalhar com json
+app.use(express.json());
+
+// Simulando usuários
+const users = [];
+
+// Criar usuários
+app.post("/usuarios", (req, res) => {
+  const { name } = req.body;
+
+  users.push(req.body);
+
+  // Ao cadastrar usuário, o front end obtém uma resposta:
+  res.send(`Usuário ${name} cadastrado com sucesso!`);
+});
+
+// Listar todos os usuários
 app.get("/usuarios", (req, res) => {
-  res.send("Está rodando");
+  res.send(users);
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+/* 
+  Criar nossa API de usuários:
+    - Criar usuários
+    - Listar todos os usuários
+    - Editar um usuário
+    - Deletar um usuário
+*/
