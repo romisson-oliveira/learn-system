8c0e3b03281f6cd7fd8b30c5f884801b4024f794
Romisson Oliveira <oliveiraromisson@hotmail.com>
2025-10-28 19:47:07 -0300

    refactor: creating users from Home/index.jsx

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,37 +1,101 @@
+import { useEffect, useState, useRef } from "react";
 import "./style.css";
-import { Trash } from "lucide-react";
+import { Trash, LoaderCircle } from "lucide-react";
+import api from "../../services/api.js";
 
 function Home() {
-  // Simulando dados do db
-  const users = [
-    {
-      id: "455253dgsdg",
-      name: "Romisson",
-      age: "32",
-      email: "rom@email.com",
-    },
-    {
-      id: "4sdasd33dgsdg",
-      name: "Viviane",
-      age: "36",
-      email: "vivi@email.com",
-    },
-    {
-      id: "4sdasbbjgdsdg",
-      name: "Paulo",
-      age: "23",
-      email: "paulo@email.com",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const getUsers = async () => {
+    const usersFromApi = await api.get("/usuarios");
+    setUsers(usersFromApi.data);
+  };
+
+  const createUser = async () => {
+    // Evita enviar com campos vazios
+    if (!name || !age || !email) {
+      alert("Por favor, preencha todos os campos!");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const response = await api.post("/usuarios", {
+        name,
+        age,
+        email,
+      });
+
+      console.log("Usuário criado:", response.data);
+      //alert("Usuário criado com sucesso!");
+
+      // Limpa os campos após sucesso
+      setName("");
+      setAge("");
+      setEmail("");
+    } catch (error) {
+      console.error("Erro ao criar usuário:", error);
+      alert("Erro ao criar usuário. Tente novamente.");
+    } finally {
+      getUsers();
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <div className="container">
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input type="text" name="nome" placeholder="Digite seu nome" />
-        <input type="number" name="idade" placeholder="Qual sua idade?" />
-        <input type="email" name="email" placeholder="Digite seu email" />
-        <button type="button">Cadastrar</button>
+        <input
+          type="text"
+          name="nome"
+          placeholder="Digite seu nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          name="idade"
+          placeholder="Qual sua idade?"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={createUser}
+          disabled={loading}
+          style={{
+            cursor: loading ? "not-allowed" : "pointer",
+          }}
+        >
+          {loading ? (
+            <LoaderCircle
+              className="animate-spin"
+              size={20}
+              strokeWidth={2.5}
+            />
+          ) : (
+            "Criar Usuário"
+          )}
+        </button>
       </form>
 
       {users.map((user) => (
@@ -41,7 +105,7 @@ function Home() {
               Nome:<span> {user.name}</span>
             </p>
             <p>
-              Idade: <span> {user.age}</span>
+              Idade: <span> {String(user.age)}</span>
             </p>
             <p>
               Email: <span> {user.email}</span>
