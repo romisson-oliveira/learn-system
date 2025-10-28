4f3f439d9d19624d748a0d87f8b24b2a550cb198
Romisson Oliveira <oliveiraromisson@hotmail.com>
2025-10-28 18:12:55 -0300

    refactor: form to create users with style

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,10 +1,58 @@
 import "./style.css";
+import { Trash } from "lucide-react";
 
 function Home() {
+  // Simulando dados do db
+  const users = [
+    {
+      id: "455253dgsdg",
+      name: "Romisson",
+      age: "32",
+      email: "rom@email.com",
+    },
+    {
+      id: "4sdasd33dgsdg",
+      name: "Viviane",
+      age: "36",
+      email: "vivi@email.com",
+    },
+    {
+      id: "4sdasbbjgdsdg",
+      name: "Paulo",
+      age: "23",
+      email: "paulo@email.com",
+    },
+  ];
+
   return (
-    <>
-      <h1>Olá react</h1>
-    </>
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input type="text" name="nome" placeholder="Digite seu nome" />
+        <input type="number" name="idade" placeholder="Qual sua idade?" />
+        <input type="email" name="email" placeholder="Digite seu email" />
+        <button type="button">Cadastrar</button>
+      </form>
+
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome:<span> {user.name}</span>
+            </p>
+            <p>
+              Idade: <span> {user.age}</span>
+            </p>
+            <p>
+              Email: <span> {user.email}</span>
+            </p>
+          </div>
+          <button>
+            <Trash />
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
 
